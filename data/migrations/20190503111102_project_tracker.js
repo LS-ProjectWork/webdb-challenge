fd147d379e2d2e8bc6db25783c0d8ddc0e89b8bf
Darien Payton <darien.payton71@gmail.com>
2019-05-03 11:21:26 -0400

    added actions table

--- data/migrations/20190503111102_project_tracker.js
+++ data/migrations/20190503111102_project_tracker.js
@@ -13,8 +13,29 @@ exports.up = function(knex, Promise) {
         table
             .boolean('completed');
     })
+
+    .createTable('actions', table => {
+        table.increments().unique()
+
+        table
+            .integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE')
+        table
+            .string('description')
+
+        table
+            .string('notes')
+        table
+            .boolean('completed');
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('actions')
+    .dropTableIfExists('projects')
 };
