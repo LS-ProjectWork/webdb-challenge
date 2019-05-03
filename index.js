946f63e1c99b13d14c261980800084507e7ac052
Darien Payton <darien.payton71@gmail.com>
2019-05-03 11:30:05 -0400

    added route for actions

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const actionsRoute = require('./routers/actionsRouter');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/actions', actionsRoute)
 
 server.listen(6000, () => {
     console.log('Server is fired up on port 6000!')
